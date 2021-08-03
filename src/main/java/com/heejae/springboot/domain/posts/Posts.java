6534050ec70aefeda27d1f4d182dd7a53170d02e
heejae <jhj967878@naver.com>
2021-08-04 00:46:55 +0900

    JPA 적용해보기

---
+++ src/main/java/com/heejae/springboot/domain/posts/Posts.java
@@ -0,0 +1,34 @@
+package com.heejae.springboot.domain.posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter // 클래스 내 모든 필드의 Getter 메소드 자동생성
+@NoArgsConstructor // 기본 생성자 자동 추가
+@Entity // 테이블과 링크될 클래스임을 나타낸다.
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK 생성 규칙, GenerationType.IDENTITY auto_increment
+    private Long id;
+
+    @Column(length = 500, nullable = false) // Column 기본값을 변경하고 싶을 때 사용
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}

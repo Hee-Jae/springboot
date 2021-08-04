8d408469526ceeccb9ee39e4d186f63a6de1298e
heejae <jhj967878@naver.com>
2021-08-05 04:03:16 +0900

    등록/수정/조회 API 만들기

--- src/main/java/com/heejae/springboot/domain/posts/Posts.java
+++ src/main/java/com/heejae/springboot/domain/posts/Posts.java
@@ -1,4 +1,5 @@
 package com.heejae.springboot.domain.posts;
+import com.heejae.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,7 @@ import javax.persistence.Id;
 @Getter // 클래스 내 모든 필드의 Getter 메소드 자동생성
 @NoArgsConstructor // 기본 생성자 자동 추가
 @Entity // 테이블과 링크될 클래스임을 나타낸다.
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // PK 생성 규칙, GenerationType.IDENTITY auto_increment
@@ -30,5 +31,10 @@ public class Posts {
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
     }
 }
